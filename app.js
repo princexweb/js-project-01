3c255bef77a94291374b309ece8f4f42e670c21b
princeXweb <princexbundela@gmail.com>
2026-02-01 17:03:10 +0530

    comeback-part-1

---
+++ app.js
@@ -0,0 +1,9 @@
+let box = document.getElementById("box");
+let btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+    let c1 = Math.floor(Math.random() * 256);
+    let c2 = Math.floor(Math.random() * 256);
+    let c3 = Math.floor(Math.random() * 256);
+    box.style.backgroundColor = `rgb(${c1}, ${c2}, ${c3})`;
+    box.innerHTML = `rgb(${c1}, ${c2}, ${c3})`;
+})
